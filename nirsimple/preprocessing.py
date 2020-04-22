ce3726c8ba3c62050701b6540a93feea6a0d13e7
HanBnrd <johann.benerradi@gmail.com>
2020-04-22 17:36:51 +0100

    Add modified Beer-Lambert law

--- nirsimple/preprocessing.py
+++ nirsimple/preprocessing.py
@@ -5,11 +5,52 @@ NIRS processing made simple
 """
 
 import warnings
+from os import path
 
 import numpy as np
+import pandas as pd
 
 
 class Preprocessing():
+
+    def _extinctions(self, wavelengths):
+        # TODO: check wavelengths are different
+        # TODO: implement interpolation
+        # TODO: scale for more than two wavelengths
+        # TODO: add other extinction data sets
+        """
+        Get molar extinction coefficients for HbO and HbR corresponding to the
+        wavelengths.
+
+            Values for molar extinction coefficients in [cm-1/(moles/liter)] or
+            [cm-1/M] based on the wavelength in [nm], compiled by Scott Prahl
+            using data from W. B. Gratzer and N. Kollias.
+            (https://omlc.org/spectra/hemoglobin/summary.html)
+
+        Parameters
+        ----------
+        wavelengths : list of integers
+            The two wavelengths for which to get the molar extinction
+            coefficients.
+
+        Returns
+        -------
+        exs : array
+            numpy array of the extinction coefficients of shape (2, 2).
+        """
+        ex = []
+        if len(wavelengths) == 2 and wavelengths[0] != wavelengths[1]:
+            ex_path = path.join(path.dirname(__file__), 'data/extinctions.csv')
+            df = pd.read_csv(ex_path, header=0)
+            for wl in wavelengths:
+                try:
+                    ex.append(df[df['lambda'] == wl].values[0][1:])
+                except IndexError:
+                    raise Exception("no matching wavelength found")
+        else:
+            raise Exception("wavelengths should be 2 different values")
+        ex = np.array(ex)
+        return ex
 
     def optical_densities(self, intensities, refs=None):
         """
@@ -50,3 +91,94 @@ class Preprocessing():
         if np.any(intensities <= 0):
             warnings.warn("some intensities are negative or equal to zero")
         return delta_od
+
+    def mbll(self, delta_od, ch_names, ch_wls, ch_dpfs, ch_distances, unit):
+        """
+        Apply the modified Beer-Lambert law (MBLL) to optical density changes
+        in order to obtain concentration changes in HbO (oxy) and HbR (deoxy)
+
+            Modified Beer-Lambert law:
+            Dod_wl = e_HbO_wl*Dc_HbO*l*DPF_wl + e_HbR_wl*Dc_HbR*l*DPF_wl
+
+            With two different wavelengths we obtain a set of linear equations
+            solved with matrices:
+            [[Dod_1]  = [[e_HbO_1*l*DPF_1, e_HbR_1*l*DPF_1]  . [[Dc_HbO]
+             [Dod_2]]    [e_HbO_2*l*DPF_2, e_HbR_2*l*DPF_2]]    [Dc_HbR]]
+
+            Equivalent to:
+            [[Dc_HbO]  = [[e_HbO_1, e_HbR_1] -1 . [[Dod_1/(l*DPF_1)]
+             [DC_HbR]]    [e_HbO_2, e_HbR_2]]      [Dod_2/(l*DPF_2)]]
+
+        Parameters
+        ----------
+        delta_od : array
+            numpy array of optical density changes, relative to average
+            intensities for each channel, of shape (channels, data points).
+
+        ch_names : list of strings
+            List of channel names.
+
+        ch_wls : list of integers
+            List of channel wavelengths.
+
+        ch_dpfs : list of floats
+            List of differential pathlength factors (DPF) also called partial
+            pathlengths factors (PPF).
+
+        ch_distances : list of floats
+            List of source-detector distances for the channels.
+
+        unit : string
+            Unit for ch_distances ('cm' or 'mm').
+
+        Returns
+        -------
+        delta_c : array
+            numpy array of concentration changes, relative to average
+            concentration for each channel, of shape (channels, data points).
+
+        new_ch_names : list of strings
+            New list of channel names.
+
+        new_ch_types : list of strings
+            New list of channel types (HbO or HbR).
+        """
+        if unit == "cm":
+            pass
+        elif unit == "mm":
+            ch_distances = np.array(ch_distances) / 10
+            ch_distances = ch_distances.tolist()
+        else:
+            raise Exception("unit should be cm or mm")
+        delta_c = []
+        new_ch_names = []
+        new_ch_types = []
+        for name in np.unique(ch_names):
+            idx_1, idx_2 = [idx for idx, x in enumerate(ch_names) if x == name]
+
+            sub_delta_od = [delta_od[idx_1], delta_od[idx_2]]
+            sub_delta_od = np.swapaxes(sub_delta_od, 0, 1)  # timepoints first
+            sub_delta_od = np.expand_dims(sub_delta_od, axis=2)
+
+            ex = self._extinctions([ch_wls[idx_1], ch_wls[idx_2]])
+            ex_inv = np.linalg.inv(ex)
+            ex_inv = np.tile(ex_inv, (len(sub_delta_od), 1, 1))
+
+            pl = [ch_dpfs[idx_1]*ch_distances[idx_1],
+                  ch_dpfs[idx_2]*ch_distances[idx_2]]
+            pl = np.tile(np.array(pl), (len(sub_delta_od), 1))
+            pl = np.expand_dims(pl, axis=2)
+
+            sub_delta_c = np.matmul(ex_inv, sub_delta_od/pl)
+            sub_delta_c = np.swapaxes(sub_delta_c, 0, 1)  # channels first
+
+            delta_c.append(sub_delta_c[0])
+            delta_c.append(sub_delta_c[1])
+            new_ch_names.append(name)
+            new_ch_names.append(name)
+            new_ch_types.append("hbo")
+            new_ch_types.append("hbr")
+
+        delta_c = np.array(delta_c)
+        delta_c = np.squeeze(delta_c)
+        return delta_c, new_ch_names, new_ch_types
