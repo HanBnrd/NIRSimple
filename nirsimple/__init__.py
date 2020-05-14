79a101abf3ef65c90d1029d84ae2d4be97be0f38
HanBnrd <johann.benerradi@gmail.com>
2020-05-14 13:19:55 +0100

    Add processing with CBSI method (Closes #2)

--- nirsimple/__init__.py
+++ nirsimple/__init__.py
@@ -7,3 +7,4 @@ fNIRS signal processing made simple
 from .preprocessing import intensities_to_od_changes
 from .preprocessing import od_to_od_changes
 from .preprocessing import mbll
+from .processing import cbsi
---
+++ nirsimple/processing.py
@@ -0,0 +1,64 @@
+"""
+NIRSimple
+---------
+fNIRS signal processing made simple
+"""
+
+import numpy as np
+
+
+def cbsi(delta_c, ch_names, ch_types):
+    """
+    Apply correlation based signal improvement (from Cui at al., 2010).
+
+        Correlation based signal improvement (CBSI):
+        x_0 = (1/2)*(x-alpha*x)
+        y_0 = -(1/alpha)*x_0
+
+    Parameters
+    ----------
+    delta_c : array
+        numpy array of concentration changes in [moles/liter] or [M] for each
+        channel, of shape (channels, data points).
+
+    ch_names : list of strings
+        List of channel names.
+
+    ch_types : list of strings
+        List of channel types ('hbo' or 'hbr').
+
+    Returns
+    -------
+    delta_c_0 : array
+        numpy array of corrected activation signals in [moles/liter] or [M] for
+        each channel, of shape (channels, data points).
+
+    new_ch_names : list of strings
+        New list of channel names.
+
+    new_ch_types : list of strings
+        New list of channel types ('hbo' or 'hbr').
+    """
+    delta_c_0 = []
+    new_ch_names = []
+    new_ch_types = []
+    for name in np.unique(ch_names):
+        idx_hbo = [idx for idx, x in enumerate(ch_names)
+                   if x == name and ch_types[idx] == 'hbo']
+        idx_hbr = [idx for idx, x in enumerate(ch_names)
+                   if x == name and ch_types[idx] == 'hbr']
+
+        alpha = np.std(delta_c[idx_hbo]) / np.std(delta_c[idx_hbr])
+        delta_c_0_hbo = (delta_c[idx_hbo] - alpha*delta_c[idx_hbr]) / 2
+        delta_c_0_hbr = -delta_c_0_hbo / alpha
+
+        delta_c_0.append(delta_c_0_hbo)
+        delta_c_0.append(delta_c_0_hbr)
+        new_ch_names.append(name)
+        new_ch_names.append(name)
+        new_ch_types.append('hbo')
+        new_ch_types.append('hbr')
+
+    delta_c_0 = np.array(delta_c_0)
+    delta_c_0 = np.squeeze(delta_c_0)
+    return delta_c_0, new_ch_names, new_ch_types
